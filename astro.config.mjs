e9d84b8040a2d1375186f566b033e7804656f4a0
lunar_commands <tfeabp@gmail.com>
2023-10-13 15:27:32 +0200

    Fixing the astro.config.mjs file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lunarcommands.github.io',
-  integrations: [svelte(), tailwind(), sitemap(), vue()],
-  headers: {
-    'Cache-Control': 'max-age=31536000'
-  }
-});
+	site: 'https://lunarcommands.github.io',
+	integrations: [tailwind(), sitemap(), vue()],
+	headers: {
+		'Cache-Control': 'max-age=31536000'
+	}
+})
